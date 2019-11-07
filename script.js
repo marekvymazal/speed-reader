df9bb78b62a4f3e3c0f4dcddddbb92407df16fba
Marek Vymazal <marekvymazal@gmail.com>
2019-11-06 19:13:35 -0800

    Add total time to read and some code cleanup

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 var go = document.getElementById('go');
 var textInput = document.getElementById('input-text');
 var textDisplay = document.getElementById('text-display');
-var wpm = document.getElementById("wpm");
+var readTime = document.getElementById('read-time');
+var wpm = document.getElementById('wpm');
 
 var ms = 1000;
 var i = 0;
@@ -16,6 +17,90 @@ var pausePunctuations = [
     "?"
 ];
 
+function isWordEndOfSentence( word ){
+    let lastChar = word.slice(-1);
+    return pausePunctuations.includes(lastChar);
+}
+
+function getWordWait( word ){
+    let lastChar = word.slice(-1);
+    let isEndOfSentence = pausePunctuations.includes(lastChar);
+    if (isEndOfSentence) {
+        // wait twice as long for end of sentence words
+        return ms * 2;
+    }
+    return ms;
+}
+
+function getTotalTime( milliseconds ){
+
+    let rs = "";
+
+    let ms = parseInt((milliseconds % 1000) / 100);
+    let seconds = Math.floor((milliseconds / 1000) % 60);
+    let minutes = Math.floor((milliseconds / (1000 * 60)) % 60);
+    let hours = Math.floor((milliseconds / (1000 * 60 * 60)) % 24);
+
+    rs += "The following can be read in "
+
+    if ( hours > 0 ){
+        rs += " " + hours + " hour";
+        if ( hours > 1){ rs += "s"; }
+    }
+
+    if ( minutes > 0 ){
+        rs += " " + minutes + " minute";
+        if ( minutes > 1){ rs += "s"; }
+    }
+
+    // only show seconds if there are no hours
+    // and minutes is less than 15
+    if (hours == 0 && minutes < 15){
+        if ( seconds > 0 ){
+            rs += " " + seconds + " second";
+            if ( seconds > 1){ rs += "s"; }
+        }
+    }
+
+    // only show milliseconds if less than a second total
+    if (milliseconds < 1000){
+        rs += " less than a second.";
+    }
+
+    return rs;
+}
+
+function getWordArray( paragraph )
+{
+    // remove new lines
+    let text = textInput.value.replace(/\r?\n|\r/g, "");
+
+    // remove tabs
+    text = text.replace('\t','');
+
+    // split into array and filter empty strings
+    return text.split(" ").filter(function(el){
+        return el != "";
+    });
+}
+
+function updated(){
+    ms = (60 / wpm.value) * 1000;
+
+    let time = 0;
+    let tempArray = getWordArray( textInput.value );
+    tempArray.forEach( function( word ){
+        time += getWordWait( word );
+    });
+    if (time <= 0 ){
+        readTime.innerHTML = "";
+        return;
+    }
+    //time = time / 1000;
+    readTime.innerHTML = getTotalTime( time );
+    //readTime.innerHTML = time.toFixed(2) + ' seconds to read';
+}
+
 function processWord(){
     if (i >= wordArray.length)
     {
@@ -27,13 +112,9 @@ function processWord(){
     textDisplay.innerHTML = wordArray[i];
 
     // check if word is end of sentence
-    let lastChar = wordArray[i].slice(-1);
-    let hasPunctuation = pausePunctuations.includes(lastChar);
-
     let wait = ms;
-    if (hasPunctuation) {
-        // wait twice as long for end of sentence words
-        wait *= 2
+    if (isWordEndOfSentence(wordArray[i])) {
+        wait *= 2 // wait twice as long for end of sentence words
     }
 
     console.log(wait, wordArray[i]);
@@ -48,16 +129,24 @@ function processWord(){
 // setup go button
 go.onclick = function(){
 
-    clearTimeout(process);
-
-    ms = (60 / wpm.value) * 1000;
     console.log(wpm.value);
     console.log("milliseconds per word:", ms);
-    console.log("pressed go button");
-    console.log(textInput.value);
 
-    wordArray = textInput.value.split(" ");
+    // stop current process
+    clearTimeout(process);
+
+    // split into array and filter empty strings
+    wordArray = getWordArray(textInput.value);
+
+    // reset word index
     i = 0;
 
+     // start process
     process = processWord();
 };
+
+wpm.onchange = updated;
+textInput.oninput = updated;
+
+// initialize values
+updated();
